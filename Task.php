ae7732f6694bdf43b98fd39290ab2f7ce03564ba
Mohammed Irshedat <m7md.irshedat94@gmail.com>
2019-01-29 03:12:01 +0200

    Declare Class

--- Task.php
+++ Task.php
@@ -1,4 +1,23 @@
 <?php
 
 
+class roundGrade {
+
+	public $Grade;
+
+	public static function gradingStudents ($g) {
+
+		$this->Grade = $g;
+
+
+
+
+	} 
+
+}
+
+$Grade = array(4, 73, 67, 38, 33);
+echo roundGrade::gradingStudents($Grade); 
+
+
 ?>
