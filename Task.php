d92d3a9d0db1a9c1485b5ca2856c2466c8a73c69
Mohammed Irshedat <m7md.irshedat94@gmail.com>
2019-01-29 05:11:20 +0200

    Write code and tested

--- Task.php
+++ Task.php
@@ -1,23 +1,36 @@
 <?php
 
+class roundGrades {
 
-class roundGrade {
+	public static function gradingStudents ($grades) {
 
-	public $Grade;
+	    $roundedGrades = [];
 
-	public static function gradingStudents ($g) {
+    	for ($i = 0; $i < count($grades); $i++) {
 
-		$this->Grade = $g;
+        	if ($grades[$i] % 5 > 2 && $grades[$i] > 37 && $grades[$i] <= 100) {
 
+            	$newGrade = $grades[$i] + (5 - ($grades[$i] % 5));
+        	} 	
 
+        	else {
 
+            	$newGrade = $grades[$i];  
+        	}
 
-	} 
+        	array_push($roundedGrades, $newGrade);
+    	}
 
+    	return $roundedGrades;
+    }     
 }
 
-$Grade = array(4, 73, 67, 38, 33);
-echo roundGrade::gradingStudents($Grade); 
+$grades = array(73, 67, 38, 33); 
+$newGrades = roundGrades::gradingStudents($grades); 
 
+for ($i= 0; $i < count($grades); $i++) {
+
+    echo "Your grade is: " .$grades[$i]. " After the round it will be: " .$newGrades[$i]. "<br>";
+}
 
 ?>
